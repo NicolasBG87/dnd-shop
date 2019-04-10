c9a7532f338e76f19e45bf780eae1df1c182642d
Nikola Bojanovic <nikolabojanovicmob@gmail.com>
2019-04-10 13:30:16 +0200

    Updates

---
+++ precache-manifest.f8bb7da20d0d2654abdcd5c4da9f05d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "033113e9f2117d5ea5bd",
+    "url": "/dnd-shop/static/js/runtime~main.a48a2977.js"
+  },
+  {
+    "revision": "c0b381691f77313c7f88",
+    "url": "/dnd-shop/static/js/main.db0b08ac.chunk.js"
+  },
+  {
+    "revision": "31ae52982c22838df05b",
+    "url": "/dnd-shop/static/js/2.63e2bf38.chunk.js"
+  },
+  {
+    "revision": "c0b381691f77313c7f88",
+    "url": "/dnd-shop/static/css/main.9a2ed20e.chunk.css"
+  },
+  {
+    "revision": "8480f6d624686950a66bf65e26eefc6f",
+    "url": "/dnd-shop/index.html"
+  }
+];
